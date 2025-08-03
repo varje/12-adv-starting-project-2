aea99b89dd9958d00d8f456e4675671e679f3548
varje <varjekivila@gmail.com>
2025-08-03 15:50:47 +0300

    add events nav

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,7 +1,11 @@
+import { Link, useParams } from 'react-router-dom';
+
 function EventDetailPage() {
+  const params = useParams();
   return (
     <>
       <h1>My Event Details Page</h1>
+      <p>{params.eventId}</p>
     </>
   );
 }
--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
+import EventsNavigation from '../components/EventsNavigation';
 
 function RootLayout() {
   return (
     <>
       <MainNavigation />
+      <EventsNavigation />
       <main>
         <Outlet />
       </main>
