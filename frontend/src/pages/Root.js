ef93b9fd0288b2bb6df1717e52037db7f90db6b8
varje <varjekivila@gmail.com>
2025-08-05 11:29:52 +0300

    create response

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,12 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
-import EventsNavigation from '../components/EventsNavigation';
 
 function RootLayout() {
+  const navigation = useNavigation();
   return (
     <>
       <MainNavigation />
       <main>
+        {navigation.state === 'loading' && <p>Loading...</p>}
         <Outlet />
       </main>
     </>
