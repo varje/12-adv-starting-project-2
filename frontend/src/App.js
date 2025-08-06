4314511915a170360337f37ba5b0d1bbd5535ef3
varje <varjekivila@gmail.com>
2025-08-06 16:53:53 +0300

    add event detail page content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventDetailPage from './pages/EventDetail';
+import EventDetailPage, {loader as eventDetailLoader} from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
@@ -53,6 +53,7 @@ const router = createBrowserRouter(
             {
               path: ':eventId',
               element: <EventDetailPage />,
+              loader: eventDetailLoader
             },
             {
               path: 'new',
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,13 +1,24 @@
-import { useParams } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
+import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useLoaderData();
   return (
     <>
-      <h1>My Event Details Page</h1>
-      <p>Event ID: {params.eventId}</p>
+      <EventItem event={data.event}></EventItem>
     </>
   );
 }
 
 export default EventDetailPage;
+export async function loader({request, params}) {
+  const id = params.eventId;
+  const response = await fetch('http://localhost:8080/events/' + id);
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+      status: 500,
+    });
+  } else {
+    return response;
+  }
+}
